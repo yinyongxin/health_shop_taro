aed66d35cc9a535d7926f42b9178d1ef56ef9f9c
银永鑫 <1101278643@qq.com>
2025-09-07 22:20:16 +0800

    refactor: 优化主题样式加载顺序
    - 移除了 app.css 中的 tailwind.css 和 taroify.css 引用
    - 在 app.ts 中将 index.css 替换为 tailwind.css
    - 这些更改可以确保主题样式正确加载，避免继承 __dx_style 类的样式

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 import { useLaunch } from "@tarojs/taro";
 import { useAppUserStore } from "./stores";
 import "./app.css";
-import "./styles/index.css";
+import "./styles/tailwind.css";
 
 function App({ children }: PropsWithChildren<any>) {
   const appUser = useAppUserStore();
