9c00955d94071e8b822cc91bd658f2aa0d8c3dd0
yinyongxin0220 <yinyongxin0220@gmail.com>
2025-09-09 08:01:29 +0000

    update

---
+++ src/pages/settlement/settlement.tsx
@@ -0,0 +1,10 @@
+import { BasePage } from "@/components"
+import { View } from "@tarojs/components"
+
+export default () => {
+  return (
+    <BasePage>
+      <View>结算</View>
+    </BasePage>
+  )
+}
