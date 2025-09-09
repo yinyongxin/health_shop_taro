f78e10380dc6eeb1a0d6c4e02fe551e2f62729d6
yinyongxin0220 <yinyongxin0220@gmail.com>
2025-09-09 06:22:07 +0000

    update

--- src/stores/useAppUserStore.ts
+++ src/stores/useAppUserStore.ts
@@ -1,17 +1,8 @@
-import { PatientDto, UserDto } from "@/client";
 import { createAppStore } from "./base";
 
 interface AppUserState {
   tabActive: string;
   updateTabActive: (value: string) => void;
-  userInfo?: UserDto | null;
-  updateUserInfo: (value: UserDto) => void;
-  patientList: PatientDto[];
-  updatePatientList: (value: PatientDto[]) => void;
-  currentPatient?: PatientDto | null;
-  setCurrentPatient: (value: PatientDto) => void;
-  /** 复诊列表 */
-  subsequentVisitList: any[];
 }
 
 export const useAppUserStore = createAppStore<AppUserState>(
@@ -19,22 +10,6 @@ export const useAppUserStore = createAppStore<AppUserState>(
     tabActive: "home",
     updateTabActive: (tabActive) => {
       set({ tabActive });
-    },
-    userInfo: null,
-    patientList: [],
-    currentPatient: null,
-    subsequentVisitList: [],
-    updateUserInfo: (value: UserDto) => {
-      set({ userInfo: value });
-    },
-    updatePatientList: (value: PatientDto[]) => {
-      if (value.length === 0) {
-        set({ currentPatient: value[0] });
-      }
-      set({ patientList: value });
-    },
-    setCurrentPatient: (value: PatientDto) => {
-      set({ currentPatient: value });
     },
   }),
   "appUser",
