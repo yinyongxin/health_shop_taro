9b79461359d1c8c2e73d816efd83cb1c2e3d18c8
银永鑫 <1101278643@qq.com>
2025-09-09 20:22:31 +0800

    update

--- src/stores/useAppUserStore.ts
+++ src/stores/useAppUserStore.ts
@@ -1,21 +1,20 @@
-import { wareListMock } from "@/mock";
-import { createAppStore } from "./base";
-import Taro from "@tarojs/taro";
+import { showToast } from "@tarojs/taro";
 import { calculateTotalPrice } from "@/utils/price";
+import { createAppStore } from "./base";
 
 export type CartListItem = {
-  id: string,
-  num: number
-}
+  id: string;
+  num: number;
+};
 
 interface AppUserState {
   tabActive: string;
   updateTabActive: (value: string) => void;
-  totalPrice: number,
-  cartList: CartListItem[]
-  addCart: (id: string) => void,
-  deleteCard: (id: string) => void
-  updateCartNum: (id: string, num: number) => void,
+  totalPrice: number;
+  cartList: CartListItem[];
+  addCart: (id: string) => void;
+  deleteCard: (id: string) => void;
+  updateCartNum: (id: string, num: number) => void;
 }
 
 export const useAppUserStore = createAppStore<AppUserState>(
@@ -27,42 +26,48 @@ export const useAppUserStore = createAppStore<AppUserState>(
     totalPrice: 0,
     cartList: [],
     addCart: (id) => {
-      const currentList = get().cartList
+      const currentList = get().cartList;
       const isIn = currentList.some((item) => item.id === id);
       if (isIn) {
-        Taro.showToast({
-          title: '请勿重复添加',
-          icon: 'error',
-          duration: 2000
-        })
-        return
+        showToast({
+          title: "请勿重复添加",
+          icon: "error",
+          duration: 2000,
+        });
+        return;
       }
-      const newCartList = [...currentList, { id, num: 1 }]
+      const newCartList = [...currentList, { id, num: 1 }];
       set({
         cartList: newCartList,
-        totalPrice: calculateTotalPrice(newCartList)
+        totalPrice: calculateTotalPrice(newCartList),
       });
-      Taro.showToast({
-        title: '加入购物车成功',
-        icon: 'success',
-        duration: 2000
-      })
+      showToast({
+        title: "加入购物车成功",
+        icon: "success",
+        duration: 2000,
+      });
     },
     deleteCard: (id) => {
-      const currentList = get().cartList
+      const currentList = get().cartList;
       const index = currentList.findIndex((item) => item.id === id);
       if (index !== -1) {
         currentList.splice(index, 1);
       }
-      set({ cartList: currentList, totalPrice: calculateTotalPrice(currentList) })
+      set({
+        cartList: currentList,
+        totalPrice: calculateTotalPrice(currentList),
+      });
     },
     updateCartNum: (id, num) => {
-      const currentList = get().cartList
+      const currentList = get().cartList;
       const index = currentList.findIndex((item) => item.id === id);
       if (index !== -1) {
         currentList[index].num = num;
       }
-      set({ cartList: currentList, totalPrice: calculateTotalPrice(currentList) })
+      set({
+        cartList: currentList,
+        totalPrice: calculateTotalPrice(currentList),
+      });
     },
   }),
   "appUser",
