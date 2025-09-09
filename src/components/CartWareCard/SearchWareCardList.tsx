caadbee56e5c9c7d7abac811c298d73846822ebc
yinyongxin0220 <yinyongxin0220@gmail.com>
2025-09-09 07:08:29 +0000

    update

--- src/components/CartWareCard/SearchWareCardList.tsx
+++ src/components/CartWareCard/SearchWareCardList.tsx
@@ -19,23 +19,34 @@ export type CartWareCardListProps = {
 export const CartWareCardList = (props: CartWareCardListProps) => {
   const appUserStore = useAppUserStore()
 
-  const cartWareList = wareListMock.filter((item) => appUserStore.cartList.includes(item.id))
+  const cartWareList = wareListMock.filter((item) => appUserStore.cartList.findIndex((cartItem) => cartItem.id === item.id) !== -1)
 
   const { data = cartWareList, className, cartWareCardProps } = props;
 
-  useEffect(() => {
-    const totalPrice = cartWareList.reduce((pre, cur) => {
-      return pre + cur.price
+  const updateTotalPrice = () => {
+    const totalPrice = data.reduce((pre, cur) => {
+      const cartItem = appUserStore.cartList.find((item) => item.id === cur.id)
+      if (cartItem) {
+        return pre + cur.price * cartItem.num
+      }
+      return pre
     }, 0)
     props.totalPriceChange?.(totalPrice)
-  }, [appUserStore.cartList.length])
+  }
+
+  useEffect(() => {
+    updateTotalPrice()
+  }, [appUserStore.cartList])
   return (
     <View
       className={classNames("pb-[64px] flex flex-col gap-[24px]", className)}
     >
       {data.map((item) => (
         <SwipeCell key={item.id}>
-          <CartWareCard info={item} {...cartWareCardProps} />
+          <CartWareCard info={item} {...cartWareCardProps} numChange={(num) => {
+            appUserStore.updateCartNum(item.id, num)
+            updateTotalPrice()
+          }} />
           <SwipeCell.Actions side="right">
             <View className="flex flex-col gap-[8px] justify-center items-center px-[40px] text-red-500"
               onClick={() => {
--- src/stores/useAppUserStore.ts
+++ src/stores/useAppUserStore.ts
@@ -3,9 +3,13 @@ import { createAppStore } from "./base";
 interface AppUserState {
   tabActive: string;
   updateTabActive: (value: string) => void;
-  cartList: string[]
+  cartList: {
+    id: string,
+    num: number
+  }[]
   addCart: (id: string) => void,
   deleteCard: (id: string) => void
+  updateCartNum: (id: string, num: number) => void,
 }
 
 export const useAppUserStore = createAppStore<AppUserState>(
@@ -17,13 +21,21 @@ export const useAppUserStore = createAppStore<AppUserState>(
     cartList: [],
     addCart: (id) => {
       const currentList = get().cartList
-      set({ cartList: [...new Set([...currentList, id])] });
+      set({ cartList: [...new Set([...currentList, { id, num: 1 }])] });
     },
     deleteCard: (id) => {
       const currentList = get().cartList
-      const index = currentList.findIndex((item) => item === id);
+      const index = currentList.findIndex((item) => item.id === id);
       if (index !== -1) {
         currentList.splice(index, 1);
+      }
+      set({ cartList: currentList })
+    },
+    updateCartNum: (id, num) => {
+      const currentList = get().cartList
+      const index = currentList.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        currentList[index].num = num;
       }
       set({ cartList: currentList })
     },
