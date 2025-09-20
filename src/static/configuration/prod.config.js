b77de319d9bab6f36eb4b73ef47703c971b10dab
银永鑫 <1101278643@qq.com>
2025-09-20 21:27:29 +0800

    ``` fix(configuration): 更新生产环境配置中的API前缀
    将生产环境配置文件中的API_PREFIX值从"/zhfy"修改为空字符串,
    以解决API请求路径重复的问题。
    ```

--- src/static/configuration/prod.config.js
+++ src/static/configuration/prod.config.js
@@ -4,6 +4,6 @@ window.prodConfig = {
   ORG_ID: "800001002",
   APPID: "wxfaaff87825f44139",
   BASE_URL: "https://testpay.eh-med.com",
-  API_PREFIX: "/zhfy",
+  API_PREFIX: "",
   FILE_PREFIX: "",
 };
