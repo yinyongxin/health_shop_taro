16124f44add53f1e0c176f612eddcced2a6e869a
银永鑫 <1101278643@qq.com>
2025-09-21 19:35:43 +0800

    fix(Skeleton): 调整分类页面骨架屏布局结构
    移除硬编码的高度样式，使用 flex-1 使子元素自适应高度，
    优化骨架屏在不同屏幕尺寸下的显示效果。

--- src/pages/index/Classify/Skeleton.tsx
+++ src/pages/index/Classify/Skeleton.tsx
@@ -5,21 +5,25 @@ export const Skeleton = () => {
     <View className="flex gap-2 p-2 h-full pb-[180px]">
       <View className="flex-1 bg-gray-200 rounded-lg"></View>
       <View className="flex-3 flex flex-col gap-2">
-        <View className="flex gap-2">
-          <View className="flex-1 bg-white rounded-lg h-[280px]"></View>
-          <View className="flex-1 bg-white rounded-lg h-[280px]"></View>
+        <View className="flex gap-2 flex-1">
+          <View className="flex-1 bg-white rounded-lg"></View>
+          <View className="flex-1 bg-white rounded-lg"></View>
         </View>
-        <View className="flex gap-2">
-          <View className="flex-1 bg-white rounded-lg h-[280px]"></View>
-          <View className="flex-1 bg-white rounded-lg h-[280px]"></View>
+        <View className="flex gap-2 flex-1">
+          <View className="flex-1 bg-white rounded-lg"></View>
+          <View className="flex-1 bg-white rounded-lg"></View>
         </View>
-        <View className="flex gap-2">
-          <View className="flex-1 bg-white rounded-lg h-[280px]"></View>
-          <View className="flex-1 bg-white rounded-lg h-[280px]"></View>
+        <View className="flex gap-2 flex-1">
+          <View className="flex-1 bg-white rounded-lg"></View>
+          <View className="flex-1 bg-white rounded-lg"></View>
         </View>
-        <View className="flex gap-2">
-          <View className="flex-1 bg-white rounded-lg h-[280px]"></View>
-          <View className="flex-1 bg-white rounded-lg h-[280px]"></View>
+        <View className="flex gap-2 flex-1">
+          <View className="flex-1 bg-white rounded-lg"></View>
+          <View className="flex-1 bg-white rounded-lg"></View>
+        </View>
+        <View className="flex gap-2 flex-1">
+          <View className="flex-1 bg-white rounded-lg"></View>
+          <View className="flex-1 bg-white rounded-lg"></View>
         </View>
       </View>
     </View>
