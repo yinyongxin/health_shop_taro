80c2ff90186c79248be69c24def91ccf1cf383f6
银永鑫 <1101278643@qq.com>
2025-09-27 00:11:21 +0800

    fix(orderList): 调整骨架屏内边距并移除底部空白
    将 Skeleton 组件的内边距从 p-2 调整为 p-3，并移除了底部的 pb-[180px] 样式，
    以优化订单列表骨架屏的显示效果。

--- src/pages/orderList/Skeleton.tsx
+++ src/pages/orderList/Skeleton.tsx
@@ -2,11 +2,10 @@ import { View } from "@tarojs/components";
 
 export const Skeleton = () => {
   return (
-    <View className="flex flex-col gap-2 p-2 pb-[180px]">
-      <View className="w-full bg-gray-200 rounded-lg h-[320px]"></View>
-      <View className="w-full bg-gray-200 rounded-lg h-[320px]"></View>
-      <View className="w-full bg-gray-200 rounded-lg h-[320px]"></View>
-      <View className="w-full bg-gray-200 rounded-lg h-[320px]"></View>
+    <View className="flex flex-col gap-2 p-3">
+      <View className="w-full bg-gray-200 rounded-lg h-[340px]"></View>
+      <View className="w-full bg-gray-200 rounded-lg h-[340px]"></View>
+      <View className="w-full bg-gray-200 rounded-lg h-[340px]"></View>
     </View>
   );
 };
