cd7ba7a99ebfc3d7518f6ad0d07950709085344d
银永鑫 <1101278643@qq.com>
2025-09-27 00:05:59 +0800

    fix(orderList): 移除Skeleton组件中多余的h-full类名
    移除了src/pages/orderList/Skeleton.tsx文件中View组件上不必要的h-full
    类名，该类名可能导致布局问题。

--- src/pages/orderList/Skeleton.tsx
+++ src/pages/orderList/Skeleton.tsx
@@ -2,7 +2,7 @@ import { View } from "@tarojs/components";
 
 export const Skeleton = () => {
   return (
-    <View className="flex flex-col gap-2 p-2 h-full pb-[180px]">
+    <View className="flex flex-col gap-2 p-2 pb-[180px]">
       <View className="w-full bg-gray-200 rounded-lg h-[320px]"></View>
       <View className="w-full bg-gray-200 rounded-lg h-[320px]"></View>
       <View className="w-full bg-gray-200 rounded-lg h-[320px]"></View>
