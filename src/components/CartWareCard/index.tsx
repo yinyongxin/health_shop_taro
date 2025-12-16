94e1600d376057a8e6bc0fb40e84b5db4aac6a3e
银永鑫 <1101278643@qq.com>
2025-12-16 19:33:07 +0800

    feat(components): 添加购物车商品卡片组件
    新增 `CartWareCard` 组件，用于展示购物车中的商品信息。
    该组件支持显示商品图片、名称、SKU 标签以及价格，并可跳转至商品详情页。
    支持通过 `border` 和 `shadow` 属性控制样式表现，底部可通过 `bottom` 插槽自定义内容。

---
+++ src/components/CartWareCard/index.tsx
@@ -0,0 +1,77 @@
+import { CartItem } from "@/client";
+import { appRouter } from "@/router";
+import { View, Text } from "@tarojs/components";
+import classNames from "classnames";
+import { safeJson } from "@/utils";
+import { ReactNode } from "react";
+import { AppTag } from "../AppTag";
+import { AppImage } from "../AppImage";
+
+export type CartWareCardProps = {
+  info: CartItem;
+  border?: boolean;
+  shadow?: boolean;
+  bottom?: ReactNode;
+};
+export const CartWareCard = (props: CartWareCardProps) => {
+  const { border, shadow = true, info, bottom } = props;
+  const goToDetailPage = () => {
+    appRouter.navigateTo("wareDetail", {
+      query: {
+        id: info.productId?.toString()!,
+      },
+    });
+  };
+
+  const skuNames = Object.values(
+    safeJson.parse(info?.skuName || "", {}),
+  ) as string[];
+
+  return (
+    <View
+      className={classNames(
+        "rounded-lg  bg-white overflow-hidden flex flex-col gap-[12px]",
+      )}
+    >
+      <View
+        className={classNames("flex", {
+          "app-shadow-sm": shadow,
+          "border-2 border-gray-100": border,
+        })}
+      >
+        <View className="p-[24px] pr-[12] rounded-lg">
+          <AppImage
+            className="size-[180px] bg-gray-300 shrink-0 rounded-lg"
+            mode="aspectFill"
+            src={info?.productImage || ""}
+            onClick={() => {
+              goToDetailPage();
+            }}
+          />
+        </View>
+        <View className="flex-1 overflow-auto py-[32px] px-[24px] pl-[12px] flex flex-col justify-between">
+          <View
+            className="text-[28px] font-semibold truncate"
+            onClick={() => {
+              goToDetailPage();
+            }}
+          >
+            {info.productName}
+          </View>
+          <View className="flex gap-1">
+            {skuNames && skuNames.length > 0 && (
+              <AppTag status="secondary">{skuNames}</AppTag>
+            )}
+          </View>
+          <View className="flex justify-between items-end">
+            <View className="flex-1 text-[32px] text-rose-500">
+              <Text className="text-[24px]">¥</Text>
+              <Text className="pl-[8px]">{info.price}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      {bottom}
+    </View>
+  );
+};
