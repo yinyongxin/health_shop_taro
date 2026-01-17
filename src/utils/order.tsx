65697da2f8eb211a0e78c6acf95f2c95fb415895
银永鑫 <1101278643@qq.com>
2026-01-17 12:57:30 +0800

    update

--- src/utils/order.tsx
+++ src/utils/order.tsx
@@ -1,9 +1,21 @@
 import { PayResult } from "@/client";
 import { appToast } from ".";
 
+const getPayParams = (payData: PayResult) => {
+  const params = {
+    appId: payData.app_id, //公众号ID，由商户传入
+    timeStamp: payData.time_stamp, //时间戳，自1970年以来的秒数
+    nonceStr: payData.nonce_str, //随机串
+    package: payData.package,
+    signType: payData.sign_type, //微信签名方式：
+    paySign: payData.pay_sign, //微信签名
+  };
+  return params;
+};
+
 export const orderPayByMiniApp = (payData: PayResult) => {
   const seachParams = new URLSearchParams({
-    urlData: JSON.stringify(payData),
+    urlData: JSON.stringify(getPayParams(payData)),
   });
   const queryStr = seachParams.toString();
   wx.miniProgram.navigateTo({
@@ -19,14 +31,7 @@ export const orderPay = async (
   },
 ) => {
   const { success, fail } = options || {};
-  const params = {
-    appId: payData.app_id, //公众号ID，由商户传入
-    timeStamp: payData.time_stamp, //时间戳，自1970年以来的秒数
-    nonceStr: payData.nonce_str, //随机串
-    package: payData.package,
-    signType: payData.sign_type, //微信签名方式：
-    paySign: payData.pay_sign, //微信签名
-  };
+  const params = getPayParams(payData);
   wx.miniProgram.getEnv((getEnvRes) => {
     if (getEnvRes.miniprogram) {
       orderPayByMiniApp(payData);
