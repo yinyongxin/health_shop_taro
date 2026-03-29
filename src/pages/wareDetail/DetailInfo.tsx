8ef638c7166ddf42d75ca5c06a1ea8d9a62b6bf6
银永鑫 <1101278643@qq.com>
2026-03-29 10:49:41 +0800

    feat: 在 DetailInfo 组件中添加商品详情标题和分隔线，优化商品详情展示

--- src/pages/wareDetail/DetailInfo.tsx
+++ src/pages/wareDetail/DetailInfo.tsx
@@ -11,6 +11,11 @@ export const DetailInfo = (props: DetailInfoProps) => {
   }
   return (
     <View className="pt-[32px] px-[24px]">
+      <View className="flex-center gap-4 pb-3">
+        <View className="h-[2px] w-10 bg-sky-400"></View>
+        <View className="flex-center">商品详情</View>
+        <View className="h-[2px] w-10 bg-sky-400"></View>
+      </View>
       <RichText
         nodes={info.detailContent.replace(
           /\<img/gi,
