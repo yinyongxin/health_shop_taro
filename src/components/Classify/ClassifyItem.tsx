0e1c1b3c14226b07f9dfba3c6288c7559a184504
银永鑫 <1101278643@qq.com>
2026-03-29 13:48:36 +0800

    feat: 调整 ClassifyItem 组件的样式，增加图片和文本区域的尺寸

--- src/components/Classify/ClassifyItem.tsx
+++ src/components/Classify/ClassifyItem.tsx
@@ -18,20 +18,22 @@ export const ClassifyItem = (props: ClassifyItemProps) => {
       }}
     >
       <View className="bg-white click-effect rounded-lg flex items-center gap-2 p-2">
-        <View className="size-[100px] flex-center overflow-hidden rounded-lg bg-gray-100">
+        <View className="size-[120px] flex-center overflow-hidden rounded-lg bg-gray-100">
           {info?.logo ? (
             <AppImage
               src={info.logo || ""}
-              className="size-[100px]"
+              className="size-[120px]"
               mode="aspectFill"
             />
           ) : (
             <LucideIcon className="text-gray-200" name="image" size={50} />
           )}
         </View>
-        <View className="h-[100px] flex-1 flex flex-col gap-[8px] justify-start">
-          <View className="line-clamp-1">{info.name}</View>
-          <View className="text-gray-400 line-clamp-2 overflow-hidden">
+        <View className="h-[120px] flex-1 flex flex-col gap-[8px] justify-start">
+          <View className="line-clamp-1 text-[28px] font-bold shrink-0">
+            {info.name}
+          </View>
+          <View className="text-gray-400 line-clamp-2 overflow-hidden h-full">
             {info.description || "暂无描述"}
           </View>
         </View>
