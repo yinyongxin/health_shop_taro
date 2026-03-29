8f32caa855ac8d4754ac2cd8bd72364d63247624
银永鑫 <1101278643@qq.com>
2026-03-29 19:08:46 +0800

    feat: 优化 NewServiceBlock 组件样式，调整内边距和字体加粗

--- src/pages/wareDetail/BaseInfo.tsx
+++ src/pages/wareDetail/BaseInfo.tsx
@@ -15,39 +15,38 @@ export const BaseInfo = (props: BaseInfoProps) => {
       bgProps={{
         className: "bg-white",
       }}
+      className="px-[24px] py-[24px] flex flex-col gap-2"
     >
-      <View className="px-[24px] py-[24px] flex flex-col gap-2">
-        <View className="flex items-end gap-3 font-bold ">
-          <View className="text-rose-500 flex items-end">
-            <Text className="text-[28px]">现价</Text>
-            <Text className="text-[28px]">￥</Text>
-            <Text className="text-[48px] leading-5 text-rose-500 align-baseline">
-              {info.price}
-            </Text>
-          </View>
+      <View className="flex items-end gap-3 font-bold ">
+        <View className="text-rose-500 flex items-end">
+          <Text className="text-[28px]">现价</Text>
+          <Text className="text-[28px]">￥</Text>
+          <Text className="text-[48px] leading-5 text-rose-500 align-baseline">
+            {info.price}
+          </Text>
+        </View>
+
+        {info.originalPrice && info.originalPrice !== info.price && (
           <View className="text-gray-500">
             <Text className="text-[28px]">原价</Text>
             <Text className="text-[28px]">￥</Text>
-            {info.originalPrice && info.originalPrice !== info.price && (
-              <>
-                <Text className="text-[28px] ">{info.originalPrice}</Text>
-              </>
-            )}
+            <Text className="text-[28px] ">{info.originalPrice}</Text>
           </View>
-        </View>
-        <View className="text-[32px] font-bold">{info?.name}</View>
+        )}
+      </View>
+      <View className="text-[32px] font-bold">{info?.name}</View>
 
-        <View className="text-gray-500">
-          {info?.description || "没有任何描述"}
-        </View>
-        <View className="flex gap-[8px]">
-          <View className="flex gap-1">
-            {safeJson.parse(info?.productTags, []).map((tag) => (
-              <AppTag key={tag} status="warning">
-                {tag}
-              </AppTag>
-            ))}
-          </View>
+      <View className="text-gray-500">
+        {info?.description || "没有任何描述"}
+      </View>
+
+      <View className="flex gap-[8px]">
+        <View className="flex gap-1">
+          {safeJson.parse(info?.productTags, []).map((tag) => (
+            <AppTag key={tag} status="warning">
+              {tag}
+            </AppTag>
+          ))}
         </View>
       </View>
     </Box>
