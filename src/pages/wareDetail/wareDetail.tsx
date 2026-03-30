b6521da18ae1a63e00fe633dd33f45e9803109c6
银永鑫 <1101278643@qq.com>
2026-03-30 21:30:49 +0800

    feat: 在商品详情页面新增地址选择组件，优化用户地址选择体验

--- src/pages/wareDetail/wareDetail.tsx
+++ src/pages/wareDetail/wareDetail.tsx
@@ -270,7 +270,16 @@ const WareDetail = () => {
             </View>
           }
         >
-          <View className="px-2">{getServiceBlock()}</View>
+          <View className="px-2">
+            {getServiceBlock()}
+            <AddressSelect
+              className="py-[24px]"
+              address={currentAddress}
+              handleSelectAddress={(val) => {
+                setCurrentAddress(val);
+              }}
+            />
+          </View>
         </AppPopup>
       );
     }
