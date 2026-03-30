ea08a8176455e3d2a25fc0f8648e3df79394670a
银永鑫 <1101278643@qq.com>
2026-03-30 21:06:00 +0800

    feat: 在 AppImage 组件中新增 originalSrc 属性，增强图片源处理逻辑

--- src/components/AppImage/index.tsx
+++ src/components/AppImage/index.tsx
@@ -2,8 +2,9 @@ import { getImagePath } from "@/utils";
 import { Image, ImageProps } from "@tarojs/components";
 
 export interface AppImageProps extends ImageProps {}
+
 export const AppImage = (props: AppImageProps) => {
-  const { src, ...rest } = props;
+  const { src, originalSrc, ...rest } = props;
   const srcUrl = src.startsWith("http") ? src : getImagePath(src);
   return <Image src={src ? srcUrl : ""} {...rest} />;
 };
