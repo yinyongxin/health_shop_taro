33616804d27b451d0bb8c4bea4ad0403be235fa4
银永鑫 <1101278643@qq.com>
2026-03-30 20:31:57 +0800

    feat: 移除 token 相关逻辑，简化应用认证状态管理

--- src/stores/useAppAuthStore.ts
+++ src/stores/useAppAuthStore.ts
@@ -3,52 +3,36 @@ import { jumpWxGetCode } from "@/utils";
 import { createAppStore } from "./base";
 
 export interface AppAuthFieldsState {
-  token?: string;
   isLogged: boolean;
+  isMiniprogram: boolean;
 }
 
 interface AppAuthState extends AppAuthFieldsState {
-  updateToken: (value: string) => void;
   updateIsLogged: (value: boolean) => void;
   logout: () => void;
-  isMiniprogram: boolean;
   updateMiniprogram: (value: boolean) => void;
 }
 
 let requestInterceptorId: number | null = null;
 
 export const useAppAuthStore = createAppStore<AppAuthState>(
-  (set, get) => ({
+  (set) => ({
     isMiniprogram: false,
     updateMiniprogram: (value) => {
       set({ isMiniprogram: value });
     },
-    token: "",
-    updateToken: (value) => {
-      if (requestInterceptorId !== null) {
-        client.instance.interceptors.request.eject(requestInterceptorId);
-      }
-      requestInterceptorId = client.instance.interceptors.request.use(
-        (config) => {
-          const token = get().token;
-          if (token) {
-            config.headers.set("Authorization", `Bearer ${token}`);
-          }
-          return config;
-        },
-      );
-      set({ token: value, isLogged: true });
-    },
+
     isLogged: false,
     updateIsLogged: (value) => {
       set({ isLogged: value });
     },
+
     logout: () => {
       if (requestInterceptorId !== null) {
         client.instance.interceptors.request.eject(requestInterceptorId);
         requestInterceptorId = null;
       }
-      set({ token: "", isLogged: false });
+      set({ isLogged: false });
       jumpWxGetCode();
     },
   }),
