a4f506c545db97eeb3140db5c68ab7aa2c0d57fe
银永鑫 <1101278643@qq.com>
2026-03-30 21:06:17 +0800

    feat: 移除 AppImage 组件中的 originalSrc 属性，简化属性处理逻辑

--- src/components/AppImage/index.tsx
+++ src/components/AppImage/index.tsx
@@ -4,7 +4,7 @@ import { Image, ImageProps } from "@tarojs/components";
 export interface AppImageProps extends ImageProps {}
 
 export const AppImage = (props: AppImageProps) => {
-  const { src, originalSrc, ...rest } = props;
+  const { src, ...rest } = props;
   const srcUrl = src.startsWith("http") ? src : getImagePath(src);
   return <Image src={src ? srcUrl : ""} {...rest} />;
 };
