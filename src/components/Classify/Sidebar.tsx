a3e1f65bc6dc97b0bc5b13cb2976bfbc4b2c4422
银永鑫 <1101278643@qq.com>
2026-04-15 22:06:17 +0800

    update

--- src/components/Classify/Sidebar.tsx
+++ src/components/Classify/Sidebar.tsx
@@ -27,7 +27,7 @@ export const Sidebar = (props: SidebarProps) => {
             }}
           >
             <View
-              className={classNames("h-[80px] flex-center rounded-lg", {
+              className={classNames("h-[80px] flex-center rounded-lg p-1", {
                 "bg-primary text-white font-semibold": isActived,
               })}
             >
